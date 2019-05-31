7f48afa8bfe161c8165ad70456a16f9de1d455fe
Student <example@example.com>
2019-05-31 10:11:54 -0700

    add a test that will return age in mercury years

--- spec/galacticAgeCalculator-spec.js
+++ spec/galacticAgeCalculator-spec.js
@@ -3,11 +3,12 @@ import { AgeCalculator } from './../src/galacticAgeCalculator.js';
 describe('AgeCalculator', function() {
 
   it('should return age in Earth days', function() {
-    let letsGetAge = new AgeCalculator('1990-05-29')
+    let letsGetAge = new AgeCalculator('1990-05-29');
     expect(letsGetAge.GetAge()).toEqual(10764);
   });
 
-  // it('should return age in Mercury days', function() {
-  //
-  // }
+  it('should return age in Mercury years', function() {
+    let letsGetAge = new AgeCalculator('1995-3-15');
+    expect(letsGetAge.GetMercuryAge()).toEqual(100.73);
+  }
 });
